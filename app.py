2575506e0c3168dbf871cf67cdf0d5649d50c036
Ubuntu <ubuntu@ip-172-31-89-163.ec2.internal>
2019-12-06 17:23:52 +0000

    initial commit

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import os
-from flask import Flask 
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = 'mongodb+srv://Saffiyax:g4DxBurtrei0j0BQ@myfirstcluster-ysd9s.mongodb.net/test?retryWrites=true&w=majority'
 
+mongo = PyMongo(app)
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
