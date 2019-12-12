492531ec0a08f2d32c59119cf748eba2b2494a5a
Ubuntu <ubuntu@ip-172-31-89-163.ec2.internal>
2019-12-12 18:01:01 +0000

    added addrecipe file

--- app.py
+++ app.py
@@ -4,15 +4,17 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
-
 app.config["MONGO_DBNAME"] = 'task_manager'
 app.config["MONGO_URI"] = 'mongodb+srv://Saffiyax:g4DxBurtrei0j0BQ@myfirstcluster-ysd9s.mongodb.net/test?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
+
 @app.route('/')
+
 @app.route('/get_recipes')
 def get_recipes():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
+    
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
