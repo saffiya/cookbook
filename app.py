29a73b528b33b0d6fa93f4b8c5fabe9c74ee44d8
saffiya <saffiyax@icloud.com>
2020-01-18 11:22:23 +0000

    Created login.html and register.html files with form layout inside. Linked it to app.py and added log in to the navbar in base.html

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
 import json
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, session, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from slugify import slugify
+import bcrypt
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'cookbook'
@@ -12,16 +13,39 @@ app.config["MONGO_URI"] = 'mongodb+srv://saffiya:St4rl1ght@myfirstcluster-ysd9s.
 mongo = PyMongo(app)
 
 @app.route('/')
-
+    
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route('/login', methods=['POST'])    
+def  login():
+    users = mongo.db.users
+    login_user = users.find_one({'username' : request.form['username']})
     
-# @app.route('/get_recipes')
-# def get_recipes():
-    # return render_template("recipe.html", recipes=mongo.db.recipes.find())
+    if login_user:
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+            session['username'] = request.form['username']
+            return redirect(url_for('index.html'))
+            
+    return 'Invalid username/password combination'
 
-
+@app.route('/register', methods=['POST', 'GET']) 
+def register():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'username' : request.form['username']})
+        
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'username' : request.form['username'], 'password' : hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('index.html'))
+            
+        return 'That username already exists!'  
+        
+    return render_template('register.html')    
+    
 @app.route("/about")
 def about():
     return render_template("about.html", page_title="About Us")
