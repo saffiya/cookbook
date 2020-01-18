0f778f669838511a6af85a7f84b5fa9cf28e095b
saffiya <saffiyax@icloud.com>
2020-01-18 13:11:24 +0000

    Added log in function

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
 import json
-from flask import Flask, render_template, redirect, request, session, url_for
+from flask import Flask, render_template, redirect, request, session, url_for, flash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from slugify import slugify
-import bcrypt
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'cookbook'
@@ -17,34 +18,6 @@ mongo = PyMongo(app)
 @app.route("/")
 def index():
     return render_template("index.html")
-
-@app.route('/login', methods=['POST'])    
-def  login():
-    users = mongo.db.users
-    login_user = users.find_one({'username' : request.form['username']})
-    
-    if login_user:
-        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
-            session['username'] = request.form['username']
-            return redirect(url_for('index.html'))
-            
-    return 'Invalid username/password combination'
-
-@app.route('/register', methods=['POST', 'GET']) 
-def register():
-    if request.method == 'POST':
-        users = mongo.db.users
-        existing_user = users.find_one({'username' : request.form['username']})
-        
-        if existing_user is None:
-            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
-            users.insert({'username' : request.form['username'], 'password' : hashpass})
-            session['username'] = request.form['username']
-            return redirect(url_for('index.html'))
-            
-        return 'That username already exists!'  
-        
-    return render_template('register.html')    
     
 @app.route("/about")
 def about():
@@ -76,7 +49,110 @@ def recipes():
 @app.route("/recipes/<recipe_id>/<sluggify_url>") 
 def recipes_recipe(recipe_id, sluggify_url):
     recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
-    return render_template("recipe.html", recipe=recipe)           
+    return render_template("recipe.html", recipe=recipe)     
+    
+    # Login
+@app.route('/login', methods=['GET'])
+def login():
+	# Check if user is not logged in already
+	if 'user' in session:
+		user_in_db = mongo.db.users.find_one({"username": session['user']})
+		if user_in_db:
+			# If so redirect user to his profile
+			flash("You are logged in already!")
+			return redirect(url_for('profile', user=user_in_db['username']))
+	else:
+		# Render the page for user to be able to log in
+		return render_template("login.html")
+		
+# Check user login details from login form
+@app.route('/user_auth', methods=['POST'])
+def user_auth():
+	form = request.form.to_dict()
+	user_in_db = mongo.db.users.find_one({"username": form['username']})
+	# Check for user in database
+	if user_in_db:
+		# If passwords match (hashed / real password)
+		if check_password_hash(user_in_db['password'], form['user_password']):
+			# Log user in (add to session)
+			session['user'] = form['username']
+			# If the user is admin redirect him to admin area
+			if session['user'] == "admin":
+				return redirect(url_for('admin'))
+			else:
+				flash("You were logged in!")
+				return redirect(url_for('profile', user=user_in_db['username']))
+			
+		else:
+			flash("Wrong password or user name!")
+			return redirect(url_for('login'))
+	else:
+		flash("You must be registered!")
+		return redirect(url_for('register'))
+		
+# Sign up
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	# Check if user is not logged in already
+	if 'user' in session:
+		flash('You are already sign in!')
+		return redirect(url_for('index'))
+	if request.method == 'POST':
+		form = request.form.to_dict()
+		# Check if the password and password1 actualy match 
+		if form['user_password'] == form['user_password1']:
+			# If so try to find the user in db
+			user = mongo.db.users.find_one({"username" : form['username']})
+			if user:
+				flash(f"{form['username']} already exists!")
+				return redirect(url_for('register'))
+			# If user does not exist register new user
+			else:				
+				# Hash password
+				hash_pass = generate_password_hash(form['user_password'])
+				#Create new user with hashed password
+				mongo.db.users.insert_one(
+					{
+						'username': form['username'],
+						'email': form['email'],
+						'password': hash_pass
+					}
+				)
+				# Check if user is actualy saved
+				user_in_db = mongo.db.users.find_one({"username": form['username']})
+				if user_in_db:
+					# Log user in (add to session)
+					session['user'] = user_in_db['username']
+					return redirect(url_for('profile', user=user_in_db['username']))
+				else:
+					flash("There was a problem savaing your profile")
+					return redirect(url_for('register'))
+
+		else:
+			flash("Passwords dont match!")
+			return redirect(url_for('register'))
+		
+	return render_template("register.html")
+	
+# Log out
+@app.route('/logout')
+def logout():
+	# Clear the session
+	session.clear()
+	flash('You were logged out!')
+	return redirect(url_for('index'))
+	
+# Profile Page
+@app.route('/profile/<user>')
+def profile(user): 
+	# Check if user is logged in
+	if 'user' in session:
+		# If so get the user and pass him to template for now
+		user_in_db = mongo.db.users.find_one({"username": user})
+		return render_template('profile.html', user=user_in_db)
+	else:
+		flash("You must be logged in!")
+		return redirect(url_for('index'))
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
